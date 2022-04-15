956acacaf7f14ab5cf67d84ea5ab9278a5b574e9
Falln <isaaccole8806@gmail.com>
2022-04-15 12:05:42 -0500

    Changed default controller loading to the most recent CSV entry. Fixed CheckedOut status on startup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace ControllerManagementSystem
         Other
     }
 
-    public class Controller : IComparable
+    public partial class Controller : IComparable
     {
         public readonly ControllerType controllerType;
         public readonly string name;
@@ -115,6 +115,91 @@ namespace ControllerManagementSystem
             }
         }
 
+        public Controller(string name, ControllerType controllerType, Boolean isCheckedOut, string controllerStatus)
+        {
+            this.name = name;
+            this.controllerType = controllerType;
+            this.controllerStatus = controllerStatus;
+            this.isCheckedOut = isCheckedOut;
+            historyFile = "Controller CSV Files/" + name + " History CSV.csv";
+            try
+            {
+                //Check if the files already exists, if so, don't create a new file
+                if (!File.Exists(historyFile))
+                {
+                    //Create a new .csv file
+                    using (FileStream fs = File.Create(historyFile)) { }
+
+                    //Write the first entry in the file. This will have time/date and the owner will be CES
+                    CsvWriterSettings settings = new();
+                    settings.AppendExisting = true;
+                    using (var writer = CsvWriter.Create(historyFile, settings))
+                    {
+                        DateTime now = DateTime.Now;
+                        //Add format: Name, ControllerType, Date, Time, In or Out, Owner, Status, and Initials
+                        writer.WriteRow(name, controllerType.ToString(), now.ToShortDateString(), now.ToShortTimeString(), "Checked In", "CES", controllerStatus, "");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.ToString());
+                throw e;
+            }
+        }
+
+        public void setCheckedOut(Boolean isCheckedOut, string owner, string controllerStatus)
+        {
+            //Set the controller checked out status to the one given
+            this.isCheckedOut = isCheckedOut;
+
+            //Start the CSV writer
+            CsvWriterSettings settings = new();
+            settings.AppendExisting = true;
+            using (var writer = CsvWriter.Create(historyFile, settings))
+            {
+                DateTime now = DateTime.Now;
+                //Add a CSV entry with the checked out status as the one given
+                //Add format: Name, ControllerType, Date, Time, In or Out, Owner, Status, and Initials
+                writer.WriteRow(name, controllerType.ToString(), now.ToShortDateString(), now.ToShortTimeString(), isCheckedOut ? "Checked Out":"Checked In", owner, controllerStatus, "");
+            }
+        }
+
+        public void checkOut(string owner, string controllerStatus)
+        {
+            //Set the controller to checked out
+            this.isCheckedOut = true;
+
+            //Start the CSV writer
+            CsvWriterSettings settings = new();
+            settings.AppendExisting = true;
+            using (var writer = CsvWriter.Create(historyFile, settings))
+            {
+                DateTime now = DateTime.Now;
+                //Add a CSV entry of "checked in"
+                //Add format: Name, ControllerType, Date, Time, In or Out, Owner, Status, and Initials
+                writer.WriteRow(name, controllerType.ToString(), now.ToShortDateString(), now.ToShortTimeString(), "Checked Out", owner, controllerStatus, "");
+            }
+        }
+
+        public void checkIn(string owner, string controllerStatus)
+        {
+            //Set the controller to checked in
+            this.isCheckedOut = false;
+
+            //Start the CSV writer
+            CsvWriterSettings settings = new();
+            settings.AppendExisting = true;
+            using (var writer = CsvWriter.Create(historyFile, settings))
+            {
+                DateTime now = DateTime.Now;
+                //Add a CSV entry of "checked in"
+                //Add format: Name, ControllerType, Date, Time, In or Out, Owner, Status, and Initials
+                writer.WriteRow(name, controllerType.ToString(), now.ToShortDateString(), now.ToShortTimeString(), "Checked In", owner, controllerStatus, "");
+            }
+        }
+
+
         public int CompareTo(Controller obj)
         {
             double thisControllerNum = Double.Parse(new string(this.name.Where(Char.IsDigit).ToArray()));
@@ -149,7 +234,7 @@ namespace ControllerManagementSystem
     {
         public override Controller Deserialize(List<String> data)
         {
-            return new Controller(data[0], FromStringToControllerType(data[1]));
+            return new Controller(data[0], FromStringToControllerType(data[1]), (data[4]=="Checked Out") ? true:false, data[6]);
         }
 
         public override List<String> Serialize(Controller data)
@@ -193,6 +278,7 @@ namespace ControllerManagementSystem
 
         public MainWindow()
         {
+
             string controllerCSVDirectory = "Controller CSV Files";
             System.IO.Directory.CreateDirectory(controllerCSVDirectory);
             /*
@@ -233,7 +319,7 @@ namespace ControllerManagementSystem
                 var dataResolver = new ControllerResolver();
                 using (var reader = CsvReader<Controller>.Create(controllerCSVFile, dataResolver))
                 {
-                    var baseController = reader.ElementAt(0);
+                    var baseController = reader.Last();
                     controllerList.Add(baseController);
                 }
             }
@@ -454,9 +540,7 @@ namespace ControllerManagementSystem
                 string controllerName = nameTextBox.Text;
 
                 Controller currController = GetController(controllerTypeItem, controllerName);
-                currController.isCheckedOut = true;
-                currController.currentOwner = UsernameCheckoutBox.Text;
-
+                currController.checkOut(UsernameCheckoutBox.Text, "New");
 
                 //Update all ComboBoxes with the new status
                 int previousIndex = ControllerNumberBox.SelectedIndex;
@@ -478,8 +562,7 @@ namespace ControllerManagementSystem
                 string controllerName = nameTextBox.Text;
 
                 Controller currController = GetController(controllerTypeItem, controllerName);
-                currController.isCheckedOut = false;
-                currController.currentOwner = "";
+                currController.checkIn(UsernameCheckoutBox.Text, "New");
 
 
                 //Update all ComboBoxes with the new status
